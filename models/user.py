284a18153595ce2fecaa206fa8b6015ed144c863
abbassimedayoub <abbassimedayoub@gamil.com>
2024-03-27 18:40:13 +0100

    file

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
@@ -12,5 +12,9 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    places = relationship("Place", backref="user",
+                          cascade="all, delete",
+                          passive_deletes=True)
+    reviews = relationship("Review", backref="user",
+                           cascade="all, delete",
+                           passive_deletes=True)
