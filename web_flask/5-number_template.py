16ca0a05d3cb4afb1133fa1939ef08546ac35de1
Alexey Chernov <97944947+alexeychern0v@users.noreply.github.com>
2024-04-10 13:43:54 +0000

    n

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,7 +42,7 @@ def n_int(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def num_template(n):
     """ Displays html page if n is int """
-    return render_template("5-number.html", number=n)
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
