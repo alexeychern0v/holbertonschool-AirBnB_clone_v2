7390c46f4c3809f897948d4f1e737138e84a2cbf
Alexey Chernov <97944947+alexeychern0v@users.noreply.github.com>
2024-04-10 13:04:01 +0000

    2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,19 +6,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def index():
     """ Returns message Hello HBNB!"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """ Returns message HBNB"""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def ctext():
     """ Displays C and the text """
     text = text.replace("_", " ")
