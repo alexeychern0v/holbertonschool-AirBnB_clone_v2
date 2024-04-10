5aadcb169470e56da8d9070ca92422173e3ab234
Alexey Chernov <97944947+alexeychern0v@users.noreply.github.com>
2024-04-10 13:11:22 +0000

    3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ Returns message Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Returns message HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def ctext(text):
+    """ Displays C and the text """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pytext(text):
+    """ Displays Python and the text """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
