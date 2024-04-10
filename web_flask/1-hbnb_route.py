ac6e498fbc15b6f35056be0c6cf04c5c00e56012
Alexey Chernov <97944947+alexeychern0v@users.noreply.github.com>
2024-04-10 12:51:37 +0000

    comments

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,12 +6,15 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def index():
+    """ Returns "Hello HBNB!" """
     return "Hello HBNB!"
 
 @app.route("/hbnb", strict_slashes=False)
 def HB():
+    """ Returns "HBNB!" """
     return "HBNB!"
 
 
 if __name__ == "__main__":
+    """ Main Function """
     app.run(host="0.0.0.0", port=5000)
