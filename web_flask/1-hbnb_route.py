bb43a8af1fdd9b191e532907c3a8ba39124f858c
Alexey Chernov <97944947+alexeychern0v@users.noreply.github.com>
2024-04-10 12:54:39 +0000

    changed code

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+""" Script that starts a Flask web application """
+
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def index():
-    """ Returns "Hello HBNB!" """
+    """ Returns a string with a message Hello HBNB!"""
     return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes=False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Returns "HBNB!" """
-    return "HBNB!"
+    """ Returns a string with a message HBNB"""
+    return "HBNB"
 
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
